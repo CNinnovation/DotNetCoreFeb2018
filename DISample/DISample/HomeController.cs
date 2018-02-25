00e332ee1ea6dc672557ce25733cc432367f30c0
Christian Nagel <christian@christiannagel.com>
2018-02-25 13:42:44 +0100

    DI samples

---
+++ DISample/DISample/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DISample
+{
+    public class HomeController
+    {
+        private readonly IHelloService _helloService;
+        public HomeController(IHelloService helloService)
+        {
+            _helloService = helloService;
+        }
+        public string Index(string name)
+        {
+            return _helloService.Hello(name);
+        }
+    }
+}
---
+++ DISample/DISample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DISample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegisterServices();
+            // var controller = new HomeController(new HelloService());
+            var controller = Container.GetService<HomeController>();
+            string response = controller.Index("Matthias");
+            Console.WriteLine(response);
+        }
+
+        static void RegisterServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IHelloService, HelloService>();
+            services.AddTransient<HomeController>();
+            Container = services.BuildServiceProvider();
+        }
+
+        public static IServiceProvider Container { get; private set; }
+    }
+}
