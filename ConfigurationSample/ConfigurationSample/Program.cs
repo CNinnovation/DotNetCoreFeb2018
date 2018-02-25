7f18d8b99e5231d2fae5b774ee7ddf145ec2cf33
Christian Nagel <christian@christiannagel.com>
2018-02-25 13:42:55 +0100

    configuration sample

---
+++ ConfigurationSample/ConfigurationSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConfigurationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").AddCommandLine(args);
+            Configuration = builder.Build();
+
+            string val1 = Configuration.GetSection("DemoSection1")["Setting1"];
+            Console.WriteLine(val1);
+            string conn1 = Configuration.GetConnectionString("DBConnection1");
+            Console.WriteLine(conn1);
+        }
+
+        public static IConfigurationRoot Configuration { get; private set; }
+    }
+}
