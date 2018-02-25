d8d800c234fce1acbfbc0a436b1690f9c0794570
Christian Nagel <christian@christiannagel.com>
2018-02-25 13:44:08 +0100

    EF Core intro

---
+++ EFCoreSample/EFCoreSample/BooksContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreSample
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => base.OnConfiguring(optionsBuilder);
+    }
+}
---
+++ EFCoreSample/EFCoreSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace EFCoreSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegisterServices();
+            CreateDatabase();
+        }
+
+        private static void RegisterServices()
+        {
+            string connectionString = @"server=(localdb)\mssqllocaldb;database=BooksSample42;trusted_connection=true";
+            var services = new ServiceCollection();
+            services.AddDbContext<BooksContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            Container = services.BuildServiceProvider();
+        }
+        public static IServiceProvider Container { get; private set; }
+
+        private static void CreateDatabase()
+        {
+            var context = Container.GetService<BooksContext>();
+            bool created = context.Database.EnsureCreated();
+            Console.WriteLine($"db created {created}");
+        }
+    }
+}
